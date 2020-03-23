e76fec01738d32d2401936c2b10d355897ae7d42
toxyl <tox@novasonica.com>
2020-03-23 20:30:47 +0100

    Change to object based data - updated .gitignore - implemented CountryData object - fixed two errors in population data - added JSON file with mapping data for country names - implemented 1h caching period, eliminating the need for a cronjob - implemented world-wise change data - added historical data to data.json - fixed 0% change errors

--- data.php
+++ data.php
@@ -1,4 +1,129 @@
 <?php
+	class CountryData
+	{
+		public static $country_names_map = null;
+
+		public static function map_country_name(&$name)
+		{
+			if (self::$country_names_map == null)
+				self::$country_names_map = json_decode(file_get_contents('country_names_map.json'), true);
+
+			if (in_array($name, array_keys(self::$country_names_map)))
+				$name = self::$country_names_map["$name"];
+		}
+
+		public $name;
+		public $population;
+		public $fatalities;
+		public $fatalities_history;
+		public $infections;
+		public $infections_history;
+
+		public function __construct($country_name)
+		{
+			self::map_country_name($country_name);
+
+			$this->name = $country_name;
+			$this->population = 0;
+			$this->infections = 0;
+			$this->fatalities = 0;
+			$this->infections_history = [ ];
+			$this->fatalities_history = [ ];
+		}
+
+		public function add_population($amount) { $this->population += $amount; }
+		public function add_infections($amount) { $this->infections += $amount; }
+		public function add_fatalities($amount) { $this->fatalities += $amount; }
+		public function add_infections_to_history($date, $amount) 
+		{ 
+			if (!isset($this->infections_history[strtotime($date)]))
+				$this->infections_history[strtotime($date)] = $amount; 
+			else
+				$this->infections_history[strtotime($date)] += $amount; 
+		}
+
+		public function add_fatalities_to_history($date, $amount) 
+		{ 
+			if (!isset($this->fatalities_history[strtotime($date)]))
+				$this->fatalities_history[strtotime($date)] = $amount; 
+			else
+				$this->fatalities_history[strtotime($date)] += $amount; 
+		}
+
+		public function infection_chance($people_met) { return $this->population == 0 ? 0 : ($this->infections / $this->population) * $people_met; }
+		public function fatality_rate() { return $this->fatalities / $this->infections; }
+
+		public function infections_last()
+		{
+			ksort($this->infections_history);
+			$values = array_values($this->infections_history);
+			array_pop($values);
+			return array_pop($values);
+		}
+
+		public function infections_change()
+		{
+			ksort($this->infections_history);
+			$values = array_values($this->infections_history);
+			$i = count($values) - 1;
+			return $i < 1 || $values[$i - 1] == 0 ? 0 : ($values[$i] / $values[$i - 1]) - 1;
+		}
+
+		public function fatalities_last()
+		{
+			ksort($this->fatalities_history);
+			$values = array_values($this->fatalities_history);
+			array_pop($values);
+			return array_pop($values);
+		}
+
+		public function fatalities_change()
+		{
+			ksort($this->fatalities_history);
+			$values = array_values($this->fatalities_history);
+			$i = count($values) - 1;
+			return $i < 1 || $values[$i - 1] == 0 ? 0 : ($values[$i] / $values[$i - 1]) - 1;
+		}
+
+		public function print()
+		{
+			if ($this->population == 0 || $this->infections == 0)
+				return;
+
+			echo "---------------------------------------------\n";
+			echo $this->name . "\n";
+			echo "---------------------------------------------\n";
+			echo "Population:                         " . $this->population . "\n";
+			echo "Infections:                         " . $this->infections . " (was " . $this->infections_last() . " = " . round($this->infections_change() * 100,4). "%)\n";
+			echo "Fatalities:                         " . $this->fatalities . " (was " . $this->fatalities_last() . " = " . round($this->fatalities_change() * 100,4). "%)\n";
+			echo "Fatality:                           " . round($this->fatality_rate()*100,4) . "%\n";
+			echo "Infection chance (  1 person  met): " . round($this->infection_chance(1)*100,4) . "%\n";
+			echo "Infection chance ( 10 persons met): " . round($this->infection_chance(10)*100,4) . "%\n";
+			echo "Infection chance ( 50 persons met): " . round($this->infection_chance(50)*100,4) . "%\n";
+			echo "Infection chance (100 persons met): " . round($this->infection_chance(100)*100,4) . "%\n";
+		}
+
+		public function html_data()
+		{
+			return [
+				$this->name,
+				$this->population,
+				$this->infections,
+				$this->infections_last(),
+				round($this->infections_change() * 100, 4) . '%',
+				$this->fatalities,
+				$this->fatalities_last(),
+				round($this->fatalities_change() * 100, 4) . '%',
+				round($this->fatality_rate() * 100, 4) . '%',
+				round($this->infection_chance(1) * 100, 4) . "%",
+				round($this->infection_chance(10) * 100, 4) . "%",
+				round($this->infection_chance(50) * 100, 4) . "%",
+				round($this->infection_chance(100) * 100, 4) . "%",
+			];
+		}
+
+	}
+
 	function get_totals($file)
 	{
 		$data = json_decode(file_get_contents($file), true);
@@ -9,59 +134,25 @@
 			$c = $totals['country'];
 			$i = $totals['latest'];
 
-			if (!isset($list[$c]))
+			if (!isset($list[$c]) || !isset($list[$c]['amount']))
 			{
-				$list[$c] = $i;
+				$list[$c]['amount'] = $i;
 			}
 			else
 			{
-				$list[$c] += $i;
+				$list[$c]['amount'] += $i;
 			}
-		}
 
-		return $list;
-	}	
-
-	function get_totals_yesterday($file)
-	{
-		$data = json_decode(file_get_contents($file), true);
-		$list = [];
-		
-		foreach ($data['locations'] as $totals)
-		{
-			$c = $totals['country'];
-			$development = [];
+			if (!isset($list[$c]['history']))
+				$list[$c]['history'] = [];
 
 			foreach ($totals['history'] as $date => $amount)
 			{
-				$development[strtotime($date)] = $amount;
+				if (!isset($list[$c]['history'][$date]))
+					$list[$c]['history'][$date] = $amount;
+				else
+					$list[$c]['history'][$date] += $amount;
 			}
-
-			ksort($development);
-
-			$last = array_pop($development);
-			$prev_to_last = array_pop($development);
-			$i =  $prev_to_last == 0 ? 0 : ($last / $prev_to_last) - 1;
-
-			if (!isset($list[$c]))
-			{
-				$list[$c] = [
-					"change" => $i,
-					"yesterday" => $last,
-					"day_before_yesterday" => $prev_to_last
-				];
-			}
-			else
-			{
-				$list[$c]["yesterday"] += $last;
-				$list[$c]["day_before_yesterday"] += $prev_to_last;
-				$list[$c]["change"] = $list[$c]["day_before_yesterday"] == 0 ? 0 : ($list[$c]["yesterday"] / $list[$c]["day_before_yesterday"]) - 1;
-			}
-		}
-
-		foreach ($list as $country => &$data)
-		{
-			$data = $data['change'];
 		}
 
 		return $list;
@@ -69,49 +160,83 @@
 	
 	function make_dataset()
 	{
-		$populations = json_decode(file_get_contents('population.json'), true);
-		$fatalities = get_totals('deaths.json');
-		$fatalities_change = get_totals_yesterday('deaths.json');
-		$infections = get_totals('infected.json');
-		$infections_change = get_totals_yesterday('infected.json');
-		$fatality_rates = [];
-		$infection_chances = [];
-		$death_chances = [];
-
-		$data = [];
-		
-		foreach ($infections as $country => $amount_infections)
+		if (!file_exists('data_html.json') || filemtime('data.json') < (time() - 1000 * 60 * 60))
 		{
-			$population          	 =  $populations["$country"] ?? 0;
-			$amount_fatalities   	 =  $fatalities["$country"] ?? 0;
-			$fatality_rate       	 =  $amount_infections > 0 ? ($amount_fatalities / $amount_infections) : 0;
-			$chance_of_infection 	 =  $population > 0 ? ($amount_infections / $population) : 0;
-			$chance_of_infection_10  = ($chance_of_infection * 10);
-			$chance_of_infection_50  = ($chance_of_infection * 50);
-			$chance_of_infection_100 = ($chance_of_infection * 100);
+			file_put_contents('infected.json', file_get_contents('https://coronavirus-tracker-api.herokuapp.com/confirmed'));
+			file_put_contents('deaths.json',   file_get_contents('https://coronavirus-tracker-api.herokuapp.com/deaths'));
 
-			$data["$country"] = [
-				"population" => $population,
-				"fatalities" => $amount_fatalities,
-				"infections" => $amount_infections,
-				"fatality" => round($fatality_rate * 100, 6),
-				"chances" => [
-					"infection" => [
-						"1" => round($chance_of_infection * 100, 8),
-						"10" => round($chance_of_infection_10 * 100, 8),
-						"50" => round($chance_of_infection_50 * 100, 8),
-						"100" => round($chance_of_infection_100 * 100, 8)
-					]
-				],
-				"changes" => [
-					"infections" => round($infections_change["$country"] * 100, 8),
-					"fatalities" => round($fatalities_change["$country"] * 100, 8)
-				]
+			$populations = json_decode(file_get_contents('population.json'), true);
+			$fatalities = get_totals('deaths.json');
+			$infections = get_totals('infected.json');
+
+			$objs = [];
+			$objs["World Wide"] = new CountryData('World Wide');
+			
+			foreach ($infections as $country => $country_data)
+			{
+				$c = $country;
+				CountryData::map_country_name($country);
+				$population          	 =  $populations["$country"] ?? 0;
+				$amount_fatalities   	 =  $fatalities["$c"]['amount'] ?? 0;
+				$amount_infections   	 =  $infections["$c"]['amount'] ?? 0;
+
+				if (!isset($objs["$country"]))
+				{
+					$objs["$country"] = new CountryData($country);
+				}
+
+				$objs["$country"]->add_population($population);
+				$objs["$country"]->add_infections($amount_infections);
+				$objs["$country"]->add_fatalities($amount_fatalities);
+
+				foreach ($fatalities["$c"]['history'] as $key => $value) 
+				{
+					$objs["$country"]->add_fatalities_to_history($key, $value);
+					$objs["World Wide"]->add_fatalities_to_history($key, $value);
+				}
+
+				foreach ($infections["$c"]['history'] as $key => $value) 
+				{
+					$objs["$country"]->add_infections_to_history($key, $value);
+					$objs["World Wide"]->add_infections_to_history($key, $value);
+				}
+
+				$objs["World Wide"]->add_population($population);
+				$objs["World Wide"]->add_infections($amount_infections);
+				$objs["World Wide"]->add_fatalities($amount_fatalities);
+			}
+
+			$html_data = [];
+			$html_data[] = [ 
+				"Country",	
+				"Population",
+				"Infections<br>(current)",
+				"Infections<br>(last)",
+				"Infections<br>(change)",
+				"Fatalities<br>(current)",
+				"Fatalities<br>(last)",
+				"Fatalities<br>(change)",
+				"Fatality Rate",
+				"Infection Chance<br>(1 person met)",
+				"Infection Chance<br>(10 persons met)",
+				"Infection Chance<br>(50 persons met)",
+				"Infection Chance<br>(100 persons met)"	
 			];
-		}
-		file_put_contents('data.json', json_encode($data, JSON_PRETTY_PRINT));
-		return $data;
-	}
 	
-	make_dataset();
+			foreach ($objs as $country => $data) 
+			{
+				if ($data->population <= 0)
+					continue;
+
+				$html_data[] = $data->html_data();
+			}
+	
+			file_put_contents('data.json', json_encode($objs, JSON_PRETTY_PRINT));
+			file_put_contents('data_html.json', json_encode($html_data, JSON_PRETTY_PRINT));
+		}
+
+		return file_get_contents('data_html.json');
+	}
+
+	echo make_dataset();
 ?>
--- index.php
+++ index.php
@@ -19,102 +19,53 @@
 		You can search for multiple countries by separating the (partial) names with commas.
 		</p>
 		<?php
-			$list = json_decode(file_get_contents('data.json'), true);
+			$list = json_decode(file_get_contents('data_html.json'), true);
+			$headers = array_shift($list);
 
 			uasort($list, function($a, $b) {
-			    return $a['fatality'] <=> $b['fatality'];
+			    return $a[8] <=> $b[8];
 			});
-
-			$list = array_reverse($list);
 
 			$search = $_GET['c'] ?? '';
 			
-			echo "<table id='data'>".
-					"<thead>".
-						"<th data-sort='string'>Country<br><br><input type='text' id='search' placeholder='filter' autofocus value='$search'></input></th>".
-						"<th data-sort='int'>Population</th>".
-						"<th data-sort='int'>Infections<br>(absolute)</th>".
-						"<th data-sort='int'>Infections<br>(change)</th>".
-						"<th data-sort='int'>Fatalities<br>(absolute)</th>".
-						"<th data-sort='int'>Fatalities<br>(change)</th>".
-						"<th data-sort='float'>Fatality</th>".
-						"<th data-sort='float'>Chance Of Infection<br>(1 person met)</th>".
-						"<th data-sort='float'>Chance Of Infection<br>(10 persons met)</th>".
-						"<th data-sort='float'>Chance Of Infection<br>(50 persons met)</th>".
-						"<th data-sort='float'>Chance Of Infection<br>(100 persons met)</th>".
-					"</thead>".
-					"<tbody>";
+			echo "\n<table id='data'>\n".
+					"\t<thead>\n".
+					"\t\t<th data-sort='string'>".$headers[0]."<br><br><input type='text' id='search' placeholder='filter' autofocus value='$search'></input></th>\n".
+					"\t\t<th data-sort='int'>".$headers[1]."</th>\n".
+					"\t\t<th data-sort='int'>".$headers[2]."</th>\n".
+					"\t\t<th data-sort='int'>".$headers[3]."</th>\n".
+					"\t\t<th data-sort='float'>".$headers[4]."</th>\n".
+					"\t\t<th data-sort='int'>".$headers[5]."</th>\n".
+					"\t\t<th data-sort='int'>".$headers[6]."</th>\n".
+					"\t\t<th data-sort='float'>".$headers[7]."</th>\n".
+					"\t\t<th data-sort='float'>".$headers[8]."</th>\n".
+					"\t\t<th data-sort='float'>".$headers[9]."</th>\n".
+					"\t\t<th data-sort='float'>".$headers[10]."</th>\n".
+					"\t\t<th data-sort='float'>".$headers[11]."</th>\n".
+					"\t\t<th data-sort='float'>".$headers[12]."</th>\n".
+					"\t</thead>\n".
+					"\t<tbody>\n";
 
-			$totals = [
-				"population" => 0,
-				"infections" => 0,
-				"fatalities" => 0,
-				"fatality" => 0,
-				"chances" => [
-					"infection" => [
-						"1" => 0,
-						"10" => 0,
-						"50" => 0,
-						"100" => 0
-					]
-				],
-				"changes" => [
-					"fatalities" => 0,
-					"infections" => 0	
-				]
-			];
-
-			function print_table_row($country, $data)
+			while ($data = array_pop($list))
 			{
-				switch ("$country")
-				{
-					case "US":
-						$country = 'United States';
-						break;
-
-					case "Taiwan*":
-						$country = 'Taiwan';
-						break;
-				}
-				
-				echo "<tr>".
-					"<td>$country</td>".
-				 	"<td>" . $data["population"] . "</td>".
-				 	"<td>" . $data["infections"] . "</td>".
-					"<td>" . sprintf('%d', $data["changes"]["infections"])  . "%</td>".
-					"<td>" . $data["fatalities"] . "</td>".
-					"<td>" . sprintf('%d', $data["changes"]["fatalities"])  . "%</td>".
-					"<td>" . sprintf('%.1f', $data["fatality"])  . "%</td>".
-					"<td>" . sprintf('%.2f', $data["chances"]["infection"]["1"])  . "%</td>".
-					"<td>" . sprintf('%.2f', $data["chances"]["infection"]["10"])  . "%</td>".
-					"<td>" . sprintf('%.2f', $data["chances"]["infection"]["50"])  . "%</td>".
-					"<td>" . sprintf('%.2f', $data["chances"]["infection"]["100"])  . "%</td>".
-				 "</tr>";
+				echo "\t\t<tr>\n".
+					 "\t\t\t<td>".$data[0]."</td>\n".
+					 "\t\t\t<td>".$data[1]."</td>\n".
+					 "\t\t\t<td>".$data[2]."</td>\n".
+					 "\t\t\t<td>".$data[3]."</td>\n".
+					 "\t\t\t<td>".$data[4]."</td>\n".
+					 "\t\t\t<td>".$data[5]."</td>\n".
+					 "\t\t\t<td>".$data[6]."</td>\n".
+					 "\t\t\t<td>".$data[7]."</td>\n".
+					 "\t\t\t<td>".$data[8]."</td>\n".
+					 "\t\t\t<td>".$data[9]."</td>\n".
+					 "\t\t\t<td>".$data[10]."</td>\n".
+					 "\t\t\t<td>".$data[11]."</td>\n".
+					 "\t\t\t<td>".$data[12]."</td>\n".
+				 	 "\t\t</tr>\n";
 			}
 
-			foreach ($list as $country => $data)
-			{
-				if ($data['infections'] <= 0 || $data["population"] <= 0)
-					continue;
-
-				print_table_row($country, $data);
-
-				$totals['population'] += $data["population"];
-				$totals['infections'] += $data["infections"];
-				$totals['fatalities'] += $data["fatalities"];
-			}
-
-			$totals["fatality"] = $totals['fatalities'] / $totals['infections'];
-			$tcoi = ($totals['infections'] / $totals['population']);
-			$totals["chances"]["infection"]["1"]   =  $tcoi * 100;
-			$totals["chances"]["infection"]["10"]  = ($tcoi * 10) * 100;
-			$totals["chances"]["infection"]["50"]  = ($tcoi * 50) * 100;
-			$totals["chances"]["infection"]["100"] = ($tcoi * 100) * 100;
-			$totals["fatality"] *= 100;
-			
-			print_table_row('<b>World Wide</b>', $totals);
-	
-			echo "</tbody></table>";
+			echo "\t</tbody>\n</table>";
 		?>
 		<p>The data is retrieved using the <a href="https://github.com/ExpDev07/coronavirus-tracker-api">Coronavirus Tracker API</a> and calculated using these formulas:<br>
 			<pre>
