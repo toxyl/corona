31d433a2ae0af7bab21d763dfa392d2f4ffd2f7c
toxyl <tox@novasonica.com>
2020-03-29 16:21:24 +0200

    automated test

--- html/index.php
+++ html/index.php
@@ -1,5 +1,5 @@
 <?php
-	require_once "data.php";
+	require_once realpath(dirname(__FILE__)) . "/../data.php";
 
 	$search = preg_replace('/[^a-zA-Z-\s\',]/', '', $_GET['c'] ?? '');
 	$sort_col = preg_replace('/\D/', '', $_GET['s'] ?? 8);
