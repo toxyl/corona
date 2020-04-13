83117509dfdd7244ff3838fabf40e87bd2074597
toxyl <tox@novasonica.com>
2020-04-13 16:21:06 +0200

    Quick'n'dirty implementation of graphs

--- js/DataTable.js
+++ js/DataTable.js
@@ -53,9 +53,9 @@ class DataTable
             var changeAbs = Math.absoluteChange(last, curr);
             var change    = Math.percentageChange(last, curr);
             var daysUntilDoubled = Math.doublingInXDays(last, curr);
-            changeAbs = last.format() + ((parseFloat(changeAbs) > 0) ? '+' : '') + changeAbs.format()  + " = " + curr.format();
+            changeAbs = last.format() + ((parseFloat(changeAbs) >= 0) ? '+' : '') + changeAbs.format()  + " = " + curr.format();
 
-            return "<b>" + label + "</b>: " + changeAbs.replace(/([\-\+\=])/g, ' $1 ') + " (" + (parseFloat(change) > 0 ? '+' : '') + change.toPercent(undefined, 2) + ", doubles in approx. "+daysUntilDoubled.toFixed(2)+" days)";
+            return "<b>" + label + "</b>: " + changeAbs.replace(/([\-\+\=])/g, ' $1 ') + " (" + (parseFloat(change) > 0 ? '+' : '') + change.toPercent(undefined, 2) + (Number.isFinite(daysUntilDoubled) ? ", doubles in approx. "+daysUntilDoubled.toFixed(2)+" days" : '') + ")";
         };
 
         var country      = this.cell(row, 0).text();
@@ -69,17 +69,59 @@ class DataTable
         var fatLast      = Number(this.cell(row, 6).text());
 
         var infStats = Math.entirePopulationAffectedInXDays(population, infLast, infCurr).round(0);
-        var deaStats = Math.entirePopulationAffectedInXDays(population, fatLast, fatCurr).round(0);
+
+        var data = CoronaTracker.data[Config.alias(country.replace(/(.*)\s+\[.*?\]/g, '$1'))];
+        var dataConfirmed = [];
+        var dataDeaths = [];
+        if (data != undefined)
+        {
+            dataConfirmed = data.confirmed.total;
+            dataDeaths = data.deaths.total;
+        }
+
+        var pathConfirmed = '';
+        var pathDeaths = '';
+        var scale = Math.ceil(Config.graphWidth / (dataConfirmed.length-1));
+        var scaleH = 1 / dataConfirmed.last(1);
+        var grid = "";
+        var firstInfection = 0;
+        var firstDeath = 0;
+        var svgH = Config.graphHeight;
+        var verticalLine = function(x, color) 
+        {
+            return '<polyline points="' + x +',0 ' + x + ',' + (svgH + 2) + '" style="stroke:' + color + '; stroke-width:1; fill:none"/>';
+        };
+        var dataLine = function(points, color) 
+        {
+            return '<polyline points="' + points + '" style="stroke:' + color + '; stroke-width:2; fill:none"/>';
+        };
+
+        for (var i = 0; i < dataConfirmed.length; i++)
+        {
+            grid += verticalLine(i*scale, Config.graphColorGrid);
+            pathConfirmed += (i*scale) +',' + (svgH - (parseFloat(dataConfirmed[i]) * scaleH) * svgH + 1).round() + " ";
+            pathDeaths += (i*scale) +',' + (svgH - (parseFloat(dataDeaths[i]) * scaleH) * svgH + 1).round() + " ";
+            if (dataConfirmed[i] <= 0) firstInfection++;
+            if (dataDeaths[i] <= 0) firstDeath++;
+        }
+        firstInfection--;
+        firstDeath--;
+        
+        grid += verticalLine(firstInfection*scale, Config.graphColorConfirmed);
+        grid += verticalLine(firstDeath*scale, Config.graphColorDeaths);
 
         var tooltipText = "<b>" + country + "</b><br><br>"+
                           "<b>Population</b>: "+population.format()+" ("+(infCurr / population).toPercent()+" infected, "+(fatCurr / population).toPercent()+" died)<br>"+
                           fmt('Confirmed', infCurr, infLast) + "<br>" + 
                           fmt('Deaths', fatCurr, fatLast) + "<br>" + 
-                          "<br>";
-        if (infStats > 0)
-        {
-            tooltipText += "At the current rate the entire population would be infected in approx. " + infStats + " days.";
-        }
+                          (infStats > 0 && Number.isFinite(infStats) ? "<br>At the current rate the entire population would be infected in approx. " + infStats + " days.<br>" : '') + "<br>"+
+                          "<svg width=\""+((dataConfirmed.length-1)*scale)+"\" height=\""+(svgH+2)+"\" style=\"border: 2px solid " + Config.graphColorGrid + "\">" + 
+                            grid + 
+                            dataLine(pathDeaths, Config.graphColorDeaths) + 
+                            dataLine(pathConfirmed, Config.graphColorConfirmed) + 
+                            "</svg><br><br>" + 
+                            "First infection was registered " + (dataConfirmed.length - firstInfection) + " days ago.<br>" + 
+                            "First death was registered " + (dataConfirmed.length - firstDeath) + " days ago.<br><br>";
 
         $(row).attr('data-tooltip', tooltipText);
     }
